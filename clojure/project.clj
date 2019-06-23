5201f6ed9991fc17ba93dc7ba301325de3001daa
Randy J. Ray <rjray@blackperl.com>
2019-06-22 18:05:44 -0600

    Leiningen file for the clojure code.

---
+++ clojure/project.clj
@@ -0,0 +1,9 @@
+(defproject daily-coding-problem "0.1.0"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-cljfmt "0.6.1"]
+            [lein-kibit "0.1.6"]
+            [lein-pprint "1.2.0"]])
